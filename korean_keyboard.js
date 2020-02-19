96b121fdd109c39934616d43c0d80816d8ad0399
Jason Ko <jekb2019@gmail.com>
2020-02-20 11:10:31 +1300

    copy text and clear text button implemented

--- korean_keyboard.js
+++ korean_keyboard.js
@@ -5,14 +5,30 @@ $(function( ) {
 
   $("#copy").on('click', function(){
     copyText($("#text-screen"));
+
+  });
+  $("#copy, #clear").on('mouseout', function(){
+    showStatus("");
   })
 
+  $("#clear").on('click', function(){
+    clearText($("#text-screen"));
+  })
+  function clearText(element){
+    $("#text-screen").val("");
+    showStatus("Cleared!");
+  }
   function copyText(element){
-    $("#text-screen")
+    $("#text-screen").select();
+    document.execCommand("copy");
+    showStatus("Copied to clipboard");
+  }
+  function showStatus(text){
+    $("#status-bar").text(text)
   }
 
   function setFontSize(fontVal){
-    var fontSize = 0.5*fontVal+0.1;
+    var fontSize = 0.3*fontVal+1;
     $("#text-screen").css({
       "font-size": fontSize+"rem"
     });
