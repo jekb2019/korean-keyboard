ba2dd4e561d2c9c7105a59d84193b99ac79ab5a2
Jason Ko <jekb2019@gmail.com>
2020-02-20 11:38:39 +1300

    Seach google and Google tranlate implemented

--- korean_keyboard.js
+++ korean_keyboard.js
@@ -9,11 +9,26 @@ $(function( ) {
   });
   $("#copy, #clear").on('mouseout', function(){
     showStatus("");
-  })
-
+  });
   $("#clear").on('click', function(){
     clearText($("#text-screen"));
-  })
+  });
+  $("#search-google").on('click', function(){
+    searchGoogle($("#text-screen").val());
+  });
+  $("#search-trans").on('click', function(){
+    googleTranslateKoEn($("#text-screen").val());
+  });
+  function searchGoogle(text){
+      openInNewTab("https://www.google.com/search?q=" + text);
+  }
+  function googleTranslateKoEn(text){
+      openInNewTab("https://translate.google.com/#view=home&op=translate&sl=ko&tl=en&text=" + text);
+  }
+  function openInNewTab(url){
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
   function clearText(element){
     $("#text-screen").val("");
     showStatus("Cleared!");
@@ -21,7 +36,7 @@ $(function( ) {
   function copyText(element){
     $("#text-screen").select();
     document.execCommand("copy");
-    showStatus("Copied to clipboard");
+    showStatus("Copied to clipboard!");
   }
   function showStatus(text){
     $("#status-bar").text(text)
